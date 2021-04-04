1cae30609ef5066eefe94a42ae811af51e19ea9f
mustafabukulmez <mustafabukulmez@hotmail.com.tr>
2021-04-04 15:49:26 +0300

    Proje Yüklendi

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace LCD_simulatır
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.led0 = new System.Windows.Forms.PictureBox();
+            this.led1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.led0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.led1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 370);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // led0
+            // 
+            this.led0.Image = ((System.Drawing.Image)(resources.GetObject("led0.Image")));
+            this.led0.Location = new System.Drawing.Point(68, 483);
+            this.led0.Name = "led0";
+            this.led0.Size = new System.Drawing.Size(50, 50);
+            this.led0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.led0.TabIndex = 1;
+            this.led0.TabStop = false;
+            this.led0.WaitOnLoad = true;
+            // 
+            // led1
+            // 
+            this.led1.Image = ((System.Drawing.Image)(resources.GetObject("led1.Image")));
+            this.led1.Location = new System.Drawing.Point(12, 483);
+            this.led1.Name = "led1";
+            this.led1.Size = new System.Drawing.Size(50, 50);
+            this.led1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.led1.TabIndex = 1;
+            this.led1.TabStop = false;
+            this.led1.WaitOnLoad = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 396);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GÖSTER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1085, 240);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 545);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.led1);
+            this.Controls.Add(this.led0);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.led0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.led1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox led0;
+        private System.Windows.Forms.PictureBox led1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LCD_simulatır
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // bu proje github da yok. Şimdi gelin bu projeyi githuba yükleyelim.
+            // bu projeyi kapatacağım ve yeni bir proje açacağım (Kodsuz başlangıç yapaqcağız.)
+        }
+
+        PictureBox[,] ekransatir;
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            EkranGetir();
+        }
+        void EkranGetir()
+        { 
+            ekransatir = new PictureBox[8, 80];
+            int xpos = 0;
+            int ypos = 20;
+            int width = 8;
+            int height = 8;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 80; j++)
+                {
+                    ekransatir[i, j] = new PictureBox();
+                    panel1.Controls.Add(ekransatir[i, j]);
+                    ekransatir[i, j].Image = led0.Image;
+                    ekransatir[i, j].SetBounds(xpos, ypos, width, height);
+                    ekransatir[i, j].Show();
+                    xpos += 8;
+                }
+                xpos = 0;
+                ypos += 8;
+            }
+        }
+
+
+        public void yaz()
+        {
+            int gonder = 0;
+            int i, j;
+            char[] gelen = textBox1.Text.ToUpper().ToCharArray();
+            for (i = 0, j = 0; i < gelen.Length && j < gelen.Length; i++, j++)
+            {
+                harfyaz(gonder, gelen[j]);
+                gonder += 8;
+            }
+        }
+
+        public void harfyaz(int a, char c)
+        {
+            int x = a;
+            harfsec(c);
+            for (int i = 0; i < 8; i++)
+            {
+                x = a;
+                for (int j = 0; j < 8; j++)
+                {
+                    if (Harf[i, j] == 1)
+                    {
+                        ekransatir[i, x].Image = led1.Image;
+
+                    }
+                    else if (Harf[i, j] == 0)
+                    {
+                        ekransatir[i, x].Image = led0.Image;
+                    }
+                    x++;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 79; j++)
+                {
+                    ekransatir[i, j].Image = ekransatir[i, j + 1].Image;
+                }
+            }
+        }
+
+        int[,] Harf;
+        public void harfsec(char karakter)
+        {
+            switch (karakter)
+            {
+                case 'A':
+                    Harf = Harfler.A();
+                    break;
+                case 'B':
+                    Harf = Harfler.B();
+                    break;
+                case 'C':
+                    Harf = Harfler.C();
+                    break;
+                case 'D':
+                    Harf = Harfler.D();
+                    break;
+                case 'E':
+                    Harf = Harfler.E();
+                    break;
+                case 'F':
+                    Harf = Harfler.F();
+                    break;
+                case 'G':
+                    Harf = Harfler.G();
+                    break;
+                case 'H':
+                    Harf = Harfler.H();
+                    break;
+                case 'I':
+                    Harf = Harfler.I();
+                    break;
+                case 'J':
+                    Harf = Harfler.J();
+                    break;
+                case 'K':
+                    Harf = Harfler.K();
+                    break;
+                case 'L':
+                    Harf = Harfler.L();
+                    break;
+                case 'M':
+                    Harf = Harfler.M();
+                    break;
+                case 'N':
+                    Harf = Harfler.N();
+                    break;
+                case 'O':
+                    Harf = Harfler.O();
+                    break;
+                case 'P':
+                    Harf = Harfler.P();
+                    break;
+                case 'R':
+                    Harf = Harfler.R();
+                    break;
+                case 'S':
+                    Harf = Harfler.S();
+                    break;
+                case 'T':
+                    Harf = Harfler.T();
+                    break;
+                case 'U':
+                    Harf = Harfler.U();
+                    break;
+                case 'V':
+                    Harf = Harfler.V();
+                    break;
+                case 'Y':
+                    Harf = Harfler.Y();
+                    break;
+                case 'Z':
+                    Harf = Harfler.Z();
+                    break;
+
+
+
+
+
+
+                //case 'a':
+                //    Harf = Harfler.a();
+                //    break;
+                //case 'b':
+                //    Harf = Harfler.b();
+                //    break;
+                //case 'c':
+                //    Harf = Harfler.c();
+                //    break;
+            }
+        }
+
+        // KÜÇÜK HARFLER EKSİK O YÜZDEN SORUN OLABİLİR.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear(); 
+            EkranGetir();
+            yaz();
+        }
+    }
+}
